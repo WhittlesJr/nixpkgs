549dbb3ccc1b5fb0513a6da30df198bb1e4c0b30
Alex Whitt <alexwhitt1337@gmail.com>
2019-02-20 13:31:41 -0500

    Dokku is almost running...

--- nixos/modules/services/development/dokku.nix
+++ nixos/modules/services/development/dokku.nix
@@ -3,9 +3,14 @@
 with lib;
 
 let
-
   cfg = config.services.dokku;
-
+  dokkuLib = "/var/lib/dokku";
+  dokkuRoot = "/home/dokku";
+  logDir = "/var/log/dokku";
+  certFile = "/etc/nginx/dhparam.pem";
+  dokkuCmd = "${pkgs.dokku}/bin/dokku";
+  pluginDir = "${pkgs.dokku}/lib/core-plugins/available";
+  htmlErrorPath = "/data/nginx-vhosts/dokku-errors";
 in {
 
   options.services.dokku = {
@@ -17,26 +22,53 @@ in {
       name = "dokku";
       group = "dokku";
       home = "/home/dokku";
-      extraGroups = [ "docker" ];
+      extraGroups = [ "docker" "adm" ];
       createHome = true;
       useDefaultShell = true;
+    };
+    users.users.syslog = {
+      name = "syslog";
+      isSystemUser = true;
     };
     users.groups.dokku = {
       name = "dokku";
     };
-    security.sudo = {
-      enable = true;
-      extraRules = [{
-        users = [ "dokku" ];
-        options = [ "NOPASSWD" ];
-        commands = [
-          "systemctl reload nginx"
-          "${pkgs.nginx}/bin/nginx -t"
-          "${pkgs.nginx}/bin/nginx -t -c *"
-        ];
-      }];
+    users.groups.adm = {
+      name = "adm";
     };
 
+    environment.systemPackages = with pkgs; [
+      dokku
+      sshcommand
+      plugn
+    ];
+
+    # From plugins/00_dokku-standard/install
+    systemd.services.dokku-standard-install = {
+      description = "Dokku Standard Install";
+      enable = true;
+      script = ''
+        mkdir -p ${dokkuLib}
+
+        if [[ ! -f "${dokkuLib}/HOSTNAME" ]]; then
+          ${pkgs.inetutils}/bin/hostname -f > ${dokkuLib}/HOSTNAME
+        fi
+
+        chown dokku:dokku ${dokkuLib}/HOSTNAME
+
+        if [[ ! -f ${dokkuLib}/VHOST ]]; then
+          [[ $(${pkgs.dnsutils}/bin/dig +short "$(<"${dokkuLib}/HOSTNAME")") ]] && cp ${dokkuLib}/HOSTNAME ${dokkuLib}/VHOST
+        fi
+
+        if [[ -f ${dokkuLib}/VHOST ]]; then
+          chown dokku:dokku ${dokkuLib}/VHOST
+        fi
+      '';
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = "yes";
+      };
+    };
     systemd.services.dokku-redeploy = {
       description = "Dokku app redeploy service";
       requires = [ "docker.service" ];
@@ -45,19 +77,55 @@ in {
       serviceConfig = {
         Type = "simple";
         User = "dokku";
-        ExecStart = "${pkgs.dokku}/bin/dokku ps:restore";
-      } ;
+        ExecStart = "${dokkuCmd} ps:restore";
+      };
     };
+
+    # From plugins/20_events/install
+    systemd.services.dokku-events-install = {
+      description = "Dokku Events Install";
+      enable = true;
+      after = [ "nginx.service" "dokku-standard-install.service" ];
+      script = ''
+        echo "1"
+        mkdir -m 755 -p ${logDir}
+        echo "1.5"
+        chgrp dokku ${logDir}
+        echo "2"
+        touch ${logDir}/events.log
+        echo "2.5"
+        chgrp dokku ${logDir}/events.log
+        echo "3"
+        chmod 644 ${logDir}/events.log
+        echo "3.5"
+        mkdir -p /var/log/nginx
+        chgrp --quiet -R adm /var/log/nginx
+        echo "4"
+        [[ -f /etc/logrotate.d/nginx ]] && sed -i -e 's/create 0640 www-data dokku/create 0640 www-data adm/g' /etc/logrotate.d/nginx
+        echo "5"
+
+        mkdir -p ${dokkuLib}/data/nginx-vhosts/dokku-errors
+        echo "6"
+        cp ${pluginDir}/nginx-vhosts/templates/*-error.html ${dokkuLib}/data/nginx-vhosts/dokku-errors/.
+        echo "7"
+      '';
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = "yes";
+      };
+    };
+
     services.rsyslogd = {
       enable = true;
       extraConfig = ''
-        :syslogtag, contains, "dokku" /var/log/dokku/events.log
+        :syslogtag, contains, "dokku" ${logDir}/events.log
       '';
     };
+
     services.logrotate = {
       enable = true;
       config = ''
-        /var/log/dokku/*.log {
+        ${logDir}/*.log {
           daily
           rotate 7
           missingok
@@ -73,55 +141,106 @@ in {
       '';
     };
 
-    environment.systemPackages = with pkgs; [
-      dokku
-      sshcommand
-      plugn
-    ];
+    # From /plugins/nginx-vhosts/install
+    security.sudo = {
+      enable = true;
+      extraConfig = ''
+        %dokku ALL=(ALL) NOPASSWD:${pkgs.libudev}/bin/systemctl reload nginx, ${pkgs.nginx}/sbin/nginx -t, ${pkgs.nginx}/sbin/nginx -t -c *
+      '';
+      #extraRules = [{
+      #  users = [ "dokku" ];
+      #  commands = [
+      #    {command = "systemctl reload nginx"; options = ["NOPASSWD"];}
+      #    {command = "${pkgs.nginx}/bin/nginx -t"; options = ["NOPASSWD"];}
+      #    {command = "${pkgs.nginx}/bin/nginx -t -c *"; options = ["NOPASSWD"];}
+      #  ];
+      #}];
+    };
+
+    systemd.services.dokku-nginx-vhosts-install = {
+      description = "Dokku NGINX VHosts Install";
+      enable = true;
+      after = [ "dokku-events-install.service" ];
+      script = ''
+        # If dhparam.pem has not been created, create it the first time
+        mkdir -p /etc/nginx
+        if [[ ! -f ${certFile} ]]; then
+          ${pkgs.openssl}/bin/openssl dhparam -out ${certFile} 2048
+        fi
+      '';
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = "yes";
+      };
+    };
 
     services.nginx = {
       enable = true;
-      sslDhparam = "/etc/nginx/dhparam.pem";
+      sslDhparam = "${certFile}";
       sslCiphers = "ECDHE-RSA-AES256-GCM-SHA512:DHE-RSA-AES256-GCM-SHA512:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384";
+      virtualHosts.dokku.extraConfig = ''
+        include ${dokkuRoot}/*/nginx.conf
+        ssl_session_cache shared:SSL:20m;
+        ssl_session_timeout 1d;
+        ssl_session_tickets on;
+      '';
     };
 
+    # From plugins/scheduler-docker-local/install
+    systemd.services.dokku-retire = {
+      description = "Dokku retire service";
+      requires = [ "docker.service" ];
+      after = [ "docker.service" ];
+      wantedBy = [ "docker.service" ];
+      serviceConfig = {
+        Type = "oneshot";
+        User = "dokku";
+        ExecStart = "${dokkuCmd} ps:restore";
+      };
+    };
+    systemd.timers.dokku-retire = {
+      description = "Run dokku-retire.service every 5 minutes";
+      timerConfig = {
+        OnCalendar = "*:0/5";
+      };
+    };
+
+    # From arch installer
     systemd.services.dokku = {
       description = "Dokku Mini-PaaS";
       enable = true;
+      requires = [
+        "dokku-standard-install.service"
+        "dokku-events-install.service"
+        "dokku-nginx-vhosts-install.service"
+        "nginx.service"
+        "logrotate.service"
+        "syslog.service"
+      ];
+      after = [
+        "dokku-standard-install.service"
+        "dokku-events-install.service"
+        "dokku-nginx-vhosts-install.service"
+        "nginx.service"
+        "logrotate.service"
+        "syslog.service"
+      ];
       script = ''
-        # if dhparam.pem has not been created, create it the first time
-        if [[ ! -f /etc/nginx/dhparam.pem ]]; then
-          openssl dhparam -out /etc/nginx/dhparam.pem 2048
-        fi
-
-        export DOKKU_LIB_ROOT=/var/lib/dokku
-        export DOKKU_ROOT="/home/dokku"
-        core_plugins_src=${pkgs.dokku}/lib/core-plugins/available
-        core_plugins=$DOKKU_LIB_ROOT/core-plugins
-        plugins=$DOKKU_LIB_ROOT/plugins
-        data=$DOKKU_LIB_ROOT/data
-        ssh_dir=$DOKKU_ROOT/.ssh
-
-        touch /var/log/dokku/events.log
-
+        ssh_dir=${dokkuLib}/.ssh
         echo "Configure dokku user"
-        ${pkgs.sshcommand}/bin/sshcommand create dokku ${pkgs.dokku}/bin/dokku
-
-        echo "Setting up storage directories"
-        mkdir -p $data/storage
-        chown dokku:dokku $data $data/storage
+        ${pkgs.sshcommand}/bin/sshcommand create dokku ${dokkuCmd}
 
         echo "Ensure proper sshcommand path"
-        echo ${pkgs.dokku}/bin/dokku > "$DOKKU_ROOT/.sshcommand"
+        echo ${dokkuCmd} > "${dokkuLib}/.sshcommand"
         mkdir -p $ssh_dir
         touch $ssh_dir/authorized_keys
-        #if [[ -f .ssh/authorized_keys ]]; then
-        #  sed -i.bak 's#${pkgs.dokku}/bin/dokku#' $ssh_dir/authorized_keys
-        #  rm $ssh_dir/authorized_keys
-        #fi
+        if [[ -f .ssh/authorized_keys ]]; then
+          sed -i.bak 's#${dokkuCmd}#' $ssh_dir/authorized_keys
+          rm $ssh_dir/authorized_keys
+        fi
 
         echo "Install all core plugins"
-        #${pkgs.dokku}/bin/dokku plugin:install --core
+        #${dokkuCmd} plugin:install --core
       '';
 
       serviceConfig = {
--- pkgs/development/tools/dokku/default.nix
+++ pkgs/development/tools/dokku/default.nix
@@ -14,27 +14,40 @@ buildGoPackage rec {
     sha256 = "0vs55qrh3y3px76hp3xsznrlbynhmkfvzvbx0diig5kcv9qjs14q";
   };
 
-  buildInputs = with pkgs; [
-    makeWrapper
-    dnsutils
-    docker
-    go
-    gliderlabs-sigil
-    nettools
-    nginx
-    procfile-util
-    rsyslog
-    coreutils
-    plugn
-    sshcommand
-    libuuid
-  ];
+  buildInputs = with pkgs; [ makeWrapper ];
 
   goDeps = ./deps.nix;
 
-  patches = [ ./drv-paths.patch ];
+  patches = [ ./nixos-compat.patch ];
 
-  postInstall = ''
+  allowGoReference = true;
+  #subPackages = [
+  #  "plugins/config"
+  #  "plugins/config/src/subcommands/export"
+  #  "plugins/config/src/subcommands/set"
+  #  "plugins/config/src/subcommands/get"
+  #  "plugins/config/src/subcommands/bundle"
+  #  "plugins/config/src/subcommands/keys"
+  #  "plugins/config/src/subcommands/unset"
+  #  "plugins/config/src/commands"
+  #  "plugins/proxy"
+  #  "plugins/network/src/subcommands/set"
+  #  "plugins/network/src/subcommands/rebuild"
+  #  "plugins/network/src/subcommands/report"
+  #  "plugins/network/src/subcommands/rebuildall"
+  #  "plugins/network/src/commands"
+  #  "plugins/network/src/triggers/post-create"
+  #  "plugins/network/src/triggers/install"
+  #];
+
+  buildPhase = ''
+    runHook renameImports
+
+    cd $NIX_BUILD_TOP/go/src/${goPackagePath}/
+    GO_ARGS="" PLUGIN_MAKE_TARGET=build make go-build
+  '';
+
+  installPhase = ''
     dir=$NIX_BUILD_TOP/go/src/${goPackagePath}/
 
     # Set up plugin directories
@@ -48,6 +61,7 @@ buildGoPackage rec {
     # Copy and enable all core plugins
     cp -r $dir/plugins/* $core_plugins/available
     find $core_plugins/available -mindepth 1 -maxdepth 1 -type d -printf '%f\n' | while read -r plugin; do
+      rm -rf $core_plugins/available/$plugin/{src,vendor,*.go,Makefile,glide*,.git*}
       ln -s $core_plugins/available/$plugin $core_plugins/enabled/$plugin
       ln -s $core_plugins/available/$plugin $plugins/available/$plugin
       ln -s $core_plugins/enabled/$plugin   $plugins/enabled/$plugin
@@ -56,18 +70,16 @@ buildGoPackage rec {
     # Add version file
     echo $rev > "$bin/VERSION"
 
-    # Add hostname file
-    ${pkgs.hostname}/bin/hostname -f > "$bin/HOSTNAME"
-    ${pkgs.hostname}/bin/hostname -f > "$bin/VHOST"
-
     # Install dokku script
     mkdir -p $bin/bin
     install -Dm755 $dir/dokku $bin/bin/dokku
     patchShebangs $bin/bin/dokku
     wrapProgram $bin/bin/dokku \
-      --set DOKKU_LIB_PATH $bin/lib \
+      --set PLUGIN_PATH $plugins \
+      --set PLUGIN_CORE_PATH $core_plugins \
       --set DOKKU_DRV $bin \
       --set PATH ${lib.makeBinPath [
+        pkgs.git
         pkgs.dnsutils
         pkgs.plugn
         pkgs.docker
