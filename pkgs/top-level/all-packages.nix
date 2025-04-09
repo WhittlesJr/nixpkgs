5ad9f841169e9446d2898172f1bb164a475af0c9
Alex Whitt <alex.joseph.whitt@gmail.com>
2025-04-08 21:00:59 -0400

    deskflow: init at v1.21.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14177,6 +14177,19 @@ with pkgs;
 
   synergyWithoutGUI = synergy.override { withGUI = false; };
 
+  deskflow = qt6Packages.callPackage ../applications/misc/deskflow {
+    inherit (darwin.apple_sdk_11_0.frameworks)
+      ApplicationServices
+      Carbon
+      Cocoa
+      CoreServices
+      ScreenSaver
+      UserNotifications
+      ;
+  };
+
+  deskflowWithoutGUI = deskflow.override { withGUI = false; };
+
   tabbed = callPackage ../applications/window-managers/tabbed {
     # if you prefer a custom config, write the config.h in tabbed.config.h
     # and enable
